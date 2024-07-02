549c6f78ddcfd999fd8396b1c572a64e5128eaab
Zhassulan1 <zhkainazarov@gmail.com>
2024-07-02 23:28:03 +0500

    added langing

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,3 +1,5 @@
+
+
 import "./header.css"
 
 export default function Header() {
@@ -7,7 +9,7 @@ export default function Header() {
     <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="/" className="flex items-center">
-                <img src="LOGO" className="mr-3 h-6 sm:h-9" alt="Volumize Logo" />
+                <img src="logo.png" className="mr-3 h-6 sm:h-9" alt="Volumize Logo" />
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Volumize</span>
             </a>
             <div className="flex items-center lg:order-2">
