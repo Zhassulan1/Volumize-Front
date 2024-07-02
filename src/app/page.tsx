7f6b29580e4d0c0bc1ebb560b8b5c39c0fa16875
Zhassulan1 <zhkainazarov@gmail.com>
2024-07-02 15:31:57 +0500

    3d view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,38 @@
-import Image from "next/image";
+'use client'
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { OBJLoader } from "three/examples/jsm/Addons.js";
+import { MTLLoader } from "three/examples/jsm/Addons.js";
+import { DDSLoader } from "three-stdlib";
+import { Suspense } from "react";
 
-export default function Home() {
+THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
+
+const Scene = () => {
+  // const materials = useLoader(MTLLoader, "Poimandres.mtl");
+  const obj = useLoader(OBJLoader, "fox.obj", (loader) => {
+    // materials.preload();
+    // loader.setMaterials(materials);
+  });
+
+  console.log(obj);
+  return <primitive object={obj} scale={0.4} />;
+};
+
+// import EarthCanvas from "./components/preview";
+
+export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>
-       Volumize
-      <div className="min-h-screen">
-        <div className="bg-custom-section">
-          Call to action
-        </div>
-      </div>
-     </div>
-    </main>
+    <div className="App">
+      <Canvas className="w-full h-full">
+        <Suspense fallback={null}>
+          <Scene />
+          <OrbitControls />
+        </Suspense>
+          {/* <Environment preset="sunset" background /> */}
+      </Canvas>
+    </div>
   );
 }
