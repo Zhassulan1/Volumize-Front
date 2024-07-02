c2b64fb74ac2971ca0e76b17edd6dbb911c757d5
Zhassulan1 <zhkainazarov@gmail.com>
2024-07-02 21:53:59 +0500

    mb works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,43 @@
 'use client'
+
 import * as THREE from "three";
-import { Canvas } from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { OBJLoader } from "three/examples/jsm/Addons.js";
-import { MTLLoader } from "three/examples/jsm/Addons.js";
 import { DDSLoader } from "three-stdlib";
 import { Suspense } from "react";
 
 THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
 
 const Scene = () => {
-  // const materials = useLoader(MTLLoader, "Poimandres.mtl");
-  const obj = useLoader(OBJLoader, "fox.obj", (loader) => {
-    // materials.preload();
-    // loader.setMaterials(materials);
+  const obj = useLoader(OBJLoader, "https://volumize-bucket.s3.amazonaws.com/userobj_8000106_1719914302.04095_tmpfd7btaj2.obj", loader => {
+    loader.manager.onError = function (url: string) {
+      console.error(`There was an error loading ${url}`);
+    };
   });
 
   console.log(obj);
   return <primitive object={obj} scale={0.4} />;
 };
 
-// import EarthCanvas from "./components/preview";
-
 export default function App() {
   return (
     <div className="App">
-      <Canvas className="w-full h-full">
+      <Canvas 
+        shadows
+        className="w-full min-h-screen"
+      >
+        <ambientLight intensity={0.5} />
+        <directionalLight 
+          position={[10, 10, 10]} 
+          intensity={1} 
+          castShadow 
+        />
+
         <Suspense fallback={null}>
           <Scene />
           <OrbitControls />
         </Suspense>
-          {/* <Environment preset="sunset" background /> */}
       </Canvas>
     </div>
   );
