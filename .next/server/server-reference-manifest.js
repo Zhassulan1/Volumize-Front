1abf2ba3cfb810f42cfc9e0673eb493160b3cbd7
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-07-24 17:41:39 +0000

    deleted white themme

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6L5qEUmfOB2BAK4pukn4Zd6XX1iODAEyFfjwXqEgmAc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VrhwzmkMZMkf23dgRbVv5n2Evv28/nIPPQckAMDyWbk=\"\n}"
