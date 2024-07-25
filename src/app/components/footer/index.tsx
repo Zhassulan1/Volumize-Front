2f455421114fed6ca59d60054777cf23682d77e6
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-07-25 11:36:28 +0000

    fix: added instagram account

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -46,11 +46,11 @@ export default function Footer() {
                     <span className="text-sm text-gray-300 sm:text-center">© 2023 <a href="#">Volumize</a>. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
-                        <a href="#" className="text-gray-400 hover:text-white">
-                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                                <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd"/>
+                        <a href="https://www.instagram.com/volumize_ai/" className="text-gray-400 hover:text-white">    
+                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                <path fillRule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clipRule="evenodd"/>
                             </svg>
-                            <span className="sr-only">Facebook page</span>
+                            <span className="sr-only">Instagram page</span>
                         </a>
                         <a href="#" className="text-gray-400 hover:text-white">
                             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
