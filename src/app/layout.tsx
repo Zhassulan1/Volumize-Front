a5383cb24fad4e294c784a0fd116832b86831e1b
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-07-31 12:29:55 +0000

    refactor: unused comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,15 +23,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <!-- Google tag (gtag.js) --> */}
-        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-BRGM3T2Y3K"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-BRGM3T2Y3K');
-        </script> */}
-
           <Script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-BRGM3T2Y3K"
@@ -42,7 +33,7 @@ export default function RootLayout({
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                
+
                 gtag('config', 'G-BRGM3T2Y3K');
             `}
           </Script>
