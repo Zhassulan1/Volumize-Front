8a362d38935e5ec84c4cbe2c39f627deb3d92cb4
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-07-31 10:19:57 +0000

    feat: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Footer from "./components/layout/footer";
-import { Analytics } from "@vercel/analytics/react"
+import Script from 'next/script'
 
+import Footer from "./components/layout/footer";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+import { Analytics } from "@vercel/analytics/react"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-BRGM3T2Y3K"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BRGM3T2Y3K');
+        </script> */}
+
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-BRGM3T2Y3K"
+          />
+
+          <Script id="google-analytics">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
+            `}
+          </Script>
+
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
