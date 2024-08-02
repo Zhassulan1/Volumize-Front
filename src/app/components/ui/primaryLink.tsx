268d9f04b63b54de241529d1231119e7dd1fe41b
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-08-02 08:42:22 +0000

    style: deleted unused property

--- src/app/components/ui/primaryLink.tsx
+++ src/app/components/ui/primaryLink.tsx
@@ -1,7 +1,6 @@
 export default function PrimaryLink({ text, url }: { text: string, url: string }) {
     return (
         <a
-            type="submit"
             href={url}
             className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none hover:ring-blue-300 dark:hover:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 my-1"
         >
