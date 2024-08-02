1e3f2be7d61b016bf3a71f101efe924ca707a37d
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-08-02 08:49:24 +0000

    refactor: added one more type of primary link

--- src/app/components/layout/header/index.tsx
+++ src/app/components/layout/header/index.tsx
@@ -39,13 +39,7 @@ export default function Header({ mainBtnText }: { mainBtnText?: string }) {
                 </div>
                 <div className="flex items-center">
                     {!!mainBtnText && (
-                    // <a 
-                    //     href="/create" 
-                    //     className="text-white focus:ring-4 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-primary-800"
-                    // >
-                    //     style
-                    // </a>
-                    <PrimaryLink2 url={"/create"} text={"Get started"}></PrimaryLink2>
+                        <PrimaryLink2 url={"/create"} text={"Get started"}></PrimaryLink2>
                     )}
                     <button 
                     data-collapse-toggle="mobile-menu-2" 
